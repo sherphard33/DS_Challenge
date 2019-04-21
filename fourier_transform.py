8bff8cce1791c36b109563323f4aced543f7e3ea
phoenix_magicianGirl <phoenixaiden.sec@gmail.com>
2019-04-21 13:31:14 +0200

    Use python to code the Cooley–Tukey Fast Fourier transform algorithm from first principles.
    The Cooley Tucker Fast Forrier Transform
    The Fourier transform converts a function from time domain to frequency domain. This transformation is commonly used for audio and image processing but it is also used in various other applications. However, most of these applications work with the information in a discrete format. The discrete Fourier transform[1] (DFT) is given with the following equation:
    
    
    
    
    Here, k ranges from 0 to N-1. This formula is inefficient to compute, especially when considering real-time applications of this algorithm. In order to address this problem, the Cooley–Tukey Fast Fourier transform algorithm incorporates a few mathematical “tricks”. The first step in simplifying this equation is to split the formula into even and odd values of n as shown below:

---
+++ fourier_transform.py
@@ -0,0 +1,26 @@
+import numpy as np
+def DFT(x):
+    """Discrete Fourier Transform of the 1D array x"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(-2j * np.pi * k * n / N)
+    return np.dot(M, x)
+
+
+def FFT(x):
+    """A recursive implementation"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+
+    if N % 2 > 0:
+        raise ValueError("size of x must be a power of 2")
+    elif N <= 32:  # this cutoff should be optimized
+        return DFT(x)
+    else:
+        X_even = FFT(x[::2])
+        X_odd = FFT(x[1::2])
+        factor = np.exp(-2j * np.pi * np.arange(N) / N)
+        return np.concatenate([X_even + factor[:N / 2] * X_odd,
+                               X_even + factor[N / 2:] * X_odd])
